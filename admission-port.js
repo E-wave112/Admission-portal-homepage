e6ba285a2da4b9e6a2761c18cdaeed56bdef8e95
Esther-art-web <69162668+Esther-art-web@users.noreply.github.com>
2020-08-08 18:05:55 +0100

    Add files via upload

---
+++ admission-port.js
@@ -0,0 +1,16 @@
+
+function myFunction1(){
+    var txtArea1= document.getElementById("txt-area1");
+    txtArea1.innerHTML="Authorities of the University of Lagos (UNILAG), School of Postgraduate Studies, invite applications from suitably qualified candidates from recognized institutions for admission into the various Postgraduate programmes offered by the institution for the 2020/2021 academic session on full-time and Part-Time basis."
+
+}
+function myFunction2(){
+    var txtArea2= document.getElementById("txt-area2");
+    txtArea2.innerHTML="The Registrar, Mr. Mensah Welcomes all the returning students back to school saying Welcome back to school. We thank God for His faithfulness and watchfulness over our lives. We hope you made time to be with your friends and family, as you are aware, attendance will form part of  the semester assessment so we are all advice to resume immediately as lectures commence fully."
+
+
+}
+function myFunction3(){
+    var txtArea3= document.getElementById("txt-area3");
+    txtArea3.innerHTML= "Applying for a Masters is not necessarily the same as applying for an undergraduate programme. Although there are some similarities ,you will probably need to write a personal statement, for example :the application process very much reflects the post-graduate nature of a Masters.You will be expected to show what you learned during your Bachelors and how you on plan on using this experience to make a success of an advanced qualification."
+}
